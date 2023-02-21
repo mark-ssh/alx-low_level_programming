05d5fb8f076b8e1881a6b9a8539a1b2b54f66fce
Mark Wonah <markwonah@gmail.com>
2023-02-21 15:22:14 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - Check main.h
+ * @r: integer input
+ * Description: Returns absolute value of a number
+ * Return: Absolute value of number r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
