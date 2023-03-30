30ce7dbd16a995c194f56ae39ae4b405a8c294df
Mark Wonah <markwonah@gmail.com>
2023-03-30 14:23:10 -0700

    hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,18 @@
-section .data
-    message db 'Hello, Holberton', 0Ah ; the message to print, followed by a newline character
+extern printf
 
-section .text
-    global _start
+	section .data
+msg:		db "Hello, Holberton", 0
+fmt:		db "%s", 10, 0
 
-_start:
-    ; write the message to standard output (file descriptor 1)
-    mov rax, 1      ; system call number for write
-    mov rdi, 1      ; file descriptor 1 (stdout)
-    mov rsi, message ; address of the message to print
-    mov rdx, 16     ; length of the message, including the newline character
-    syscall         ; invoke the system call
-    
-    ; exit the program with status code 0
-    mov rax, 60     ; system call number for exit
-    xor rdi, rdi    ; exit status code 0
-    syscall         ; invoke the system call
+	section .text
 
+	global main
+main:
+	push	rbp
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0
+	call	printf
+	pop	rbp
+	mov	rax,0
+	ret
